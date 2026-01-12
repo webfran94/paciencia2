39448ebe1ff9ada5f73d403dfdc9dec886f816cb
webfran94 <franrweb@gmail.com>
2026-01-12 18:33:42 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// --- TUS CREDENCIALES REALES DE FIREBASE ---
+// TUS CREDENCIALES REALES
 const firebaseConfig = {
   apiKey: "AIzaSyDUiJh9VsQS_td82-YSyusGr-DqcJtYt7g",
   authDomain: "manual-paciencia.firebaseapp.com",
   projectId: "manual-paciencia",
   storageBucket: "manual-paciencia.firebasestorage.app",
   messagingSenderId: "759536065466",
-  appId: "1:759536065466:web:f5210ec8614f0b5b421b83",
-  // measurementId: "" // Si usas Google Analytics para Firebase, descomenta y asigna el valor.
+  appId: "1:759536065466:web:f5210ec8614f0b5b421b83"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);;
+export const db = getFirestore(app);
+
 
 
